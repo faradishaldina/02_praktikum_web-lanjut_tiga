746013b3628e6fc66c018b50218b83a803dd9c48
faradishaldina <faraldyna@gmail.com>
2023-03-10 11:05:03 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        return redirect('https://www.educastudio.com/');
+    }
+    
+    
+    // public function index() {
+    //     echo ('Home</br>');
+    //     echo ('<a href="https://www.educastudio.com/">educastudio home</a>');
+    // }
+}
